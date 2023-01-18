8d23cab2689b7e1a22aed0a382457f5a25a2499b
sddale <seth.d.dale@gmail.com>
2023-01-18 13:09:46 -0700

    Proof of concept static inheritance

--- include/csm_units/temperature.hpp
+++ include/csm_units/temperature.hpp
@@ -2,12 +2,28 @@
 
 namespace csm_units {
 
-class Temperature {
+template <class Converter>
+class Temperature : public Converter {
  public:
   constexpr explicit Temperature(double temperature) noexcept
-      : data(temperature) {}
+      : data(Converter::ConvertValue(temperature)) {}
 
   double data;
 };
+
+class KelvinConverter {
+  // how do we represent a kelvin such that we can easily convert units
+  auto ConvertValue(double value) -> double {
+    return value;  // Do nothing
+  }
+};
+
+class CelsiusConverter {
+  // how do we represent a kelvin such that we can easily convert units
+  auto ConvertValue(double value) -> double { return value + 273.15; }
+};
+
+using Kelvin = Temperature<KelvinConverter>;
+using Celsius = Temperature<CelsiusConverter>;
 
 }  // namespace csm_units
