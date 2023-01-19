eb5f4fe3cddd44c5a32dcbbdc50742289e487097
manassaini <manas_saini@yahoo.com>
2023-01-18 19:34:04 -0700

    Errors in test case w/ template - pushing for now

--- test/source/temperature_test.cpp
+++ test/source/temperature_test.cpp
@@ -4,9 +4,10 @@
 
 namespace csm_units::test {
 
+template <class Converter>
 TEST_CASE("Temperature") {
-  const auto temperature = Temperature(78);
-  CHECK(temperature.data == 78);
+  const auto temperature = Temperature(Converter 78.0);
+  CHECK(temperature.data == 78.0);
 }
 
 }  // namespace csm_units::test
