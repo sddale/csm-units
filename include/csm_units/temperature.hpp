777dd4c4dde5a945209e5b0fc32b11feca7b0d8e
manassaini <manas_saini@yahoo.com>
2023-01-21 12:09:11 -0700

    Added rounding feature and finished all test cases - all pas

--- include/csm_units/temperature.hpp
+++ include/csm_units/temperature.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 #include <iostream>
+#include <cmath>
 
 namespace csm_units {
 
@@ -16,6 +17,7 @@ class Temperature : public Converter {
 class KelvinConverter {
   public:
   constexpr auto ConvertValue(double value) noexcept -> double {
+    
     return value;  
   }
 };
@@ -23,15 +25,21 @@ class KelvinConverter {
 class CelsiusConverter {
   public:
   constexpr auto ConvertValue(double value) noexcept -> double { 
-    return value + 273.15; }
+    value = value + 273.15;                   // actual calculation
+    value = (int)(value * 1000.0) / 1000.0;   // rounding to 3 decimal places
+    
+    return value;
+    }
 };
 
 //not in clang tidy yet - still have to figure out
 class FahrenheitConverter {
   public:
     constexpr auto ConvertValue(double value) noexcept -> double {
-        return ((value - 32) / 1.8) + 273.15;
-        std::cout << value;
+        value = (value - 32) / 1.8 + 273.15;      // actual calculation
+        value = (int)(value * 1000.0) / 1000.0;   // rounding to 3 decimal places
+        
+        return value;
     }
 
 };
--- test/source/temperature_test.cpp
+++ test/source/temperature_test.cpp
@@ -8,17 +8,32 @@ TEST_CASE("Kelvin to Kelvin") {
   auto lowTemperature = Kelvin(34.0);
   CHECK(lowTemperature.data == 34.0);
   
-  auto highTemperature = Kelvin(300.0);
-  CHECK(highTemperature.data == 300.0);
+  auto highTemperature = Kelvin(300.4543);
+  CHECK(highTemperature.data == 300.4543);
 }
 
 TEST_CASE("Fahrenheit to Kelvin") {
   auto baseTemperature = Fahrenheit(32.0);
   CHECK(baseTemperature.data == 273.15);
 
-  auto lowTemperature = Fahrenheit(0.0);
-  CHECK(lowTemperature.data == 255.0);
+  auto lowTemperature = Fahrenheit(-32.554);
+  CHECK(lowTemperature.data == 237.286);
 
+  auto highTemperature = Fahrenheit(150.58483948747);
+  CHECK(highTemperature.data == 339.030);
 }
+
+TEST_CASE("Celsius to Kelvin") {
+  auto baseTemperature = Celsius(0.0);
+  CHECK(baseTemperature.data == 273.15);
+
+  auto lowTemperature = Celsius(-132.34211111);
+  CHECK(lowTemperature.data == 140.807);
+
+  auto highTemperature = Celsius(120.453);
+  CHECK(highTemperature.data == 393.602);
+}
+
+
 
 }  // namespace csm_units::test
