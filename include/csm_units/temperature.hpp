e1c5f81cf164f6adfaf11c5b479e5c314d621505
manassaini <manas_saini@yahoo.com>
2023-01-23 17:44:50 -0700

    Used clang tidy

--- include/csm_units/temperature.hpp
+++ include/csm_units/temperature.hpp
@@ -6,44 +6,41 @@ namespace csm_units {
 template <class Converter>
 class Temperature : public Converter {
  public:
-  //setting data to temperature, but had to convert it first using template and respective converter function
-  constexpr explicit Temperature(double temperature) noexcept : data(Converter::ConvertValue(temperature)) {}
-    
+  // setting data to temperature, but had to convert it first using template and
+  // respective converter function
+  constexpr explicit Temperature(double temperature) noexcept
+      : data(Converter::ConvertValue(temperature)) {}
+
   double data;
 };
 
 class KelvinConverter {
-  public:
-    constexpr auto ConvertValue(double value) noexcept -> double {
-    
-    return value;  
-  }
+ public:
+  constexpr auto ConvertValue(double value) noexcept -> double { return value; }
 };
 
 class CelsiusConverter {
-  public:
-    constexpr auto ConvertValue(double value) noexcept -> double { 
-      value = value + 273.15;                        // actual calculation
-      value = (int)(value * 1000.0) / 1000.0;        // rounding to 3 decimal places
-    
-      return value;
-    }
+ public:
+  constexpr auto ConvertValue(double value) noexcept -> double {
+    value = value + 273.15;                  // actual calculation
+    value = (int)(value * 1000.0) / 1000.0;  // rounding to 3 decimal places
+
+    return value;
+  }
 };
 
 class FahrenheitConverter {
-  public:
-    constexpr auto ConvertValue(double value) noexcept -> double {
-      value = (value - 32) / 1.8 + 273.15;           // actual calculation
-      value = (int)(value * 1000.0) / 1000.0;        // rounding to 3 decimal places
-        
-      return value;
-    }
+ public:
+  constexpr auto ConvertValue(double value) noexcept -> double {
+    value = (value - 32) / 1.8 + 273.15;     // actual calculation
+    value = (int)(value * 1000.0) / 1000.0;  // rounding to 3 decimal places
 
+    return value;
+  }
 };
 
 using Kelvin = Temperature<KelvinConverter>;
 using Celsius = Temperature<CelsiusConverter>;
 using Fahrenheit = Temperature<FahrenheitConverter>;
-
 
 }  // namespace csm_units
