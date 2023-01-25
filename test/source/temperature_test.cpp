dcff6ab811fda07c17c3954a614e7e4a1dd4391a
manassaini <manas_saini@yahoo.com>
2023-01-25 15:06:07 -0700

    Cleaned up test file and added TEST_SUITE

--- test/source/temperature_test.cpp
+++ test/source/temperature_test.cpp
@@ -6,42 +6,53 @@
 
 namespace csm_units::test {
 
-TEST_CASE("Kelvin to Kelvin") {
-  {
-    auto temperature = Kelvin(34.0);
-    CHECK(temperature.data == doctest::Approx(34.0));
+TEST_SUITE("Temperature") {
+
+  TEST_CASE("Kelvin to Kelvin") {
+    {
+      auto temperature = Kelvin(34.0);
+      CHECK(temperature.data == doctest::Approx(34.0));
+    }
+    {
+      auto temperature = Kelvin(300.4543);
+      CHECK(temperature.data == doctest::Approx(300.4543));
+    }
   }
-  {
-    auto temperature = Kelvin(300.4543);
-    CHECK(temperature.data == doctest::Approx(300.4543));
+
+  TEST_CASE("Fahrenheit to Kelvin") {
+    {
+      auto temperature = Fahrenheit(32.0);
+      CHECK(temperature.data == doctest::Approx(273.15));
+    }
+    {
+      auto temperature = Fahrenheit(-32.554);
+      CHECK(temperature.data == doctest::Approx(237.286));
+    }
+    {
+      auto temperature = Fahrenheit(150.58483948747);
+      CHECK(temperature.data == doctest::Approx(339.030));
+    }
   }
-}
 
-TEST_CASE("Fahrenheit to Kelvin") {
-  auto baseTemperature = Fahrenheit(32.0);
-  CHECK(baseTemperature.data == doctest::Approx(273.15));
+  TEST_CASE("Celsius to Kelvin") {
+    {
+      auto temperature = Celsius(0.0);
+      CHECK(temperature.data == doctest::Approx(273.15));
+    }
+    { 
+      auto temperature = Celsius(-132.34211111);
+      CHECK(temperature.data == doctest::Approx(140.807));
+    }
+    {
+      auto temperature = Celsius(120.453);
+      CHECK(temperature.data == doctest::Approx(393.602));
+    }
+  }
 
-  auto lowTemperature = Fahrenheit(-32.554);
-  CHECK(lowTemperature.data == doctest::Approx(237.286));
-
-  auto highTemperature = Fahrenheit(150.58483948747);
-  CHECK(highTemperature.data == doctest::Approx(339.030));
-}
-
-TEST_CASE("Celsius to Kelvin") {
-  auto baseTemperature = Celsius(0.0);
-  CHECK(baseTemperature.data == doctest::Approx(273.15));
-
-  auto lowTemperature = Celsius(-132.34211111);
-  CHECK(lowTemperature.data == doctest::Approx(140.807));
-
-  auto highTemperature = Celsius(120.453);
-  CHECK(highTemperature.data == doctest::Approx(393.602));
-}
-
-TEST_CASE("Size") {
-  auto temp = Kelvin(10);
-  REQUIRE(sizeof(temp) == sizeof(temp.data));  // converter requires 0 bytes
+  TEST_CASE("Size") {
+    auto temp = Kelvin(10);
+    REQUIRE(sizeof(temp) == sizeof(temp.data));  // converter requires 0 bytes
+  }
 }
 
 }  // namespace csm_units::test
