35cedbe15845a478565e7de475f42cb9b41d6ec2
manassaini <manas_saini@yahoo.com>
2023-01-25 15:31:38 -0700

    Added const correctness - apparently don't need when static? circle back later

--- test/source/temperature_test.cpp
+++ test/source/temperature_test.cpp
@@ -10,41 +10,41 @@ TEST_SUITE("Temperature") {
 
   TEST_CASE("Kelvin to Kelvin") {
     {
-      auto temperature = Kelvin(34.0);
+      const auto temperature = Kelvin(34.0);
       CHECK(temperature.data == doctest::Approx(34.0));
     }
     {
-      auto temperature = Kelvin(300.4543);
+      const auto temperature = Kelvin(300.4543);
       CHECK(temperature.data == doctest::Approx(300.4543));
     }
   }
 
   TEST_CASE("Fahrenheit to Kelvin") {
     {
-      auto temperature = Fahrenheit(32.0);
+      const auto temperature = Fahrenheit(32.0);
       CHECK(temperature.data == doctest::Approx(273.15));
     }
     {
-      auto temperature = Fahrenheit(-32.554);
+      const auto temperature = Fahrenheit(-32.554);
       CHECK(temperature.data == doctest::Approx(237.286));
     }
     {
-      auto temperature = Fahrenheit(150.58483948747);
+      const auto temperature = Fahrenheit(150.58483948747);
       CHECK(temperature.data == doctest::Approx(339.030));
     }
   }
 
   TEST_CASE("Celsius to Kelvin") {
     {
-      auto temperature = Celsius(0.0);
+      const auto temperature = Celsius(0.0);
       CHECK(temperature.data == doctest::Approx(273.15));
     }
     { 
-      auto temperature = Celsius(-132.34211111);
+      const auto temperature = Celsius(-132.34211111);
       CHECK(temperature.data == doctest::Approx(140.807));
     }
     {
-      auto temperature = Celsius(120.453);
+      const auto temperature = Celsius(120.453);
       CHECK(temperature.data == doctest::Approx(393.602));
     }
   }
