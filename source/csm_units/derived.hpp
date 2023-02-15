ac68ba968977d8d81b1ff91abbae86860fa6cfbb
SanderSchott <sschott@mines.edu>
2023-02-15 15:57:41 -0700

    Fixed some mistakes with the start of derived

--- source/csm_units/derived.hpp
+++ source/csm_units/derived.hpp
@@ -1,8 +1,16 @@
+#pragma once
+
 #include <ratio>
+
+#include "base.hpp"
+
+namespace csm_units {
 
 template <class Length, int LengthPower, class Mass, int MassPower, class Time,
           int TimePower, Arithmetic Data = double>
 class Derived {
  public:
   Data data;
+};
+
 }
