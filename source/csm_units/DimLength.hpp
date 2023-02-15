d19ea8fb6371a4ca004354254bf0658cb7a99ac4
manassaini <manas_saini@yahoo.com>
2023-02-15 16:53:00 -0700

    Added new classes and tracked them

---
+++ source/csm_units/DimLength.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace csm_units {
+
+class DimLength {
+  // Length units
+};
+
+}  // namespace csm_units
---
+++ source/csm_units/DimTime.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace csm_units {
+
+class DimTime {
+  // Length units
+};
+
+}  // namespace csm_units
