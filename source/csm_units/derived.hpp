d5e86ff6b48d33fc2f5607d1692bd56191eece90
manassaini <manas_saini@yahoo.com>
2023-02-15 17:13:04 -0700

    Added Derived class and respective MakeDerived functions

--- source/csm_units/derived.hpp
+++ source/csm_units/derived.hpp
@@ -2,6 +2,9 @@
 
 #include <ratio>
 
+#include "DimLength.hpp"
+#include "DimMass.hpp"
+#include "DimTime.hpp"
 #include "base.hpp"
 
 namespace csm_units {
@@ -10,7 +13,31 @@ template <class Length, int LengthPower, class Mass, int MassPower, class Time,
           int TimePower, Arithmetic Data = double>
 class Derived {
  public:
+  Derived(double in) : data(in) {}
   Data data;
+  // not making member variables for now and seeing if I can access them from
+  // template
+
+  template <class Conv, class Ratio>
+  static auto MakeDerived(Base<DimLength, Conv, Ratio> base) {
+    return Derived<Base<DimLength, Conv, Ratio>, 1,
+                   Base<DimMass, NoConverter, std::ratio<1, 1>>, 0,
+                   Base<DimTime, NoConverter, std::ratio<1, 1>>, 0>(base.data);
+  }
+
+  template <class Conv, class Ratio>
+  static auto MakeDerived(Base<DimTime, Conv, Ratio> base) {
+    return Derived<Base<DimLength, NoConverter, std::ratio<1, 1>>, 0,
+                   Base<DimMass, NoConverter, std::ratio<1, 1>>, 0,
+                   Base<DimTime, Conv, Ratio>, 1>(base.data);
+  }
+
+  template <class Conv, class Ratio>
+  static auto MakeDerived(Base<DimMass, Conv, Ratio> base) {
+    return Derived<Base<DimLength, NoConverter, std::ratio<1, 1>>, 0,
+                   Base<DimMass, Conv, Ratio>, 1,
+                   Base<DimTime, NoConverter, std::ratio<1, 1>>, 0>(base.data);
+  }
 };
 
-}
+}  // namespace csm_units
