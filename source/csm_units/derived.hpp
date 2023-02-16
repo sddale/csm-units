38274d810d8b026892103bd7bef25e0e6f95e07e
manassaini <manas_saini@yahoo.com>
2023-02-15 18:26:03 -0700

    Added op overloading methods - still have to handle data correctly

--- source/csm_units/derived.hpp
+++ source/csm_units/derived.hpp
@@ -38,6 +38,27 @@ class Derived {
                    Base<DimMass, Conv, Ratio>, 1,
                    Base<DimTime, NoConverter, std::ratio<1, 1>>, 0>(base.data);
   }
+
+  // operator overload methods
+  // have to convert (to proper converter methods when actually dividing)
+
+  template <class Length1, int LengthPower1, class Mass1, int MassPower1,
+            class Time1, int TimePower1,  // first object
+            class Length2, int LengthPower2, class Mass2, int MassPower2,
+            class Time2, int TimePower2>  // second object
+  auto operator/(
+      Derived<Length1, LengthPower1, Mass1, MassPower1, Time1, TimePower1> lhs,
+      Derived<Length2, LengthPower2, Mass2, MassPower2, Time2, TimePower2> rhs)
+       return Derived<Length1, LengthPower1 - LengthPower2, Mass1,
+                 MassPower1 - MassPower2, Time1, TimePower1 - TimePower2>(lhs.data / rhs.data);
+
+
+  template <class Length1, int LengthPower1, class Mass1, int MassPower1,
+          class Time1, int TimePower1, class T>
+  auto operator/(
+    Derived<Length1, LengthPower1, Mass1, MassPower1, Time1, TimePower1> lhs,  // understand this
+    T rhs) {                                                                   // doing compound / base
+  return lhs / DerivedFactory::MakeDerived(rhs); 
 };
 
 }  // namespace csm_units
