e292be1b60b2a3c05778e0b257da1c53f247f0ca
sddale <seth.d.dale@gmail.com>
2023-03-06 13:50:21 -0700

    Corrected forward references, fixed return types for +-
    Co-authored-by: Sander <SanderSchott@users.noreply.github.com>
    Co-authored-by: Manas Saini <manassaini@users.noreply.github.com>

--- source/csm_units/derived.hpp
+++ source/csm_units/derived.hpp
@@ -39,6 +39,26 @@ struct Factory {
     return Derived<Base<DimLength>, 0, Base<DimMass, Conv, Ratio, Data>, 1,
                    Base<DimTime>, 0, Data>(std::forward<decltype(base)>(base));
   }
+  // template <class Conv, class Ratio, Arithmetic Data>
+  // static auto Make(Base<DimLength, Conv, Ratio, Data>& base) {
+  //   return Derived<Base<DimLength, Conv, Ratio, Data>, 1, Base<DimMass>, 0,
+  //                  Base<DimTime>, 0,
+  //                  Data>(std::forward<decltype(base)>(base));
+  // }
+
+  // template <class Conv, class Ratio, Arithmetic Data>
+  // static auto Make(Base<DimTime, Conv, Ratio, Data>& base) {
+  //   return Derived<Base<DimLength>, 0, Base<DimMass>, 0,
+  //                  Base<DimTime, Conv, Ratio, Data>, 1, Data>(
+  //       std::forward<decltype(base)>(base));
+  // }
+
+  // template <class Conv, class Ratio, Arithmetic Data>
+  // static auto Make(Base<DimMass, Conv, Ratio, Data>& base) {
+  //   return Derived<Base<DimLength>, 0, Base<DimMass, Conv, Ratio, Data>, 1,
+  //                  Base<DimTime>, 0,
+  //                  Data>(std::forward<decltype(base)>(base));
+  // }
 };
 
 }  // namespace derived
@@ -55,16 +75,23 @@ class Derived {
 
   // copy constructor
   constexpr Derived(const Derived& other) noexcept = default;
+  constexpr Derived(const BaseType auto& other) noexcept : data(other.data){};
 
   // move constructor
-  constexpr Derived(const Derived&& other) noexcept = default;
+  constexpr Derived(Derived&& other) noexcept = default;
+
+  // NOLINTBEGIN(bugprone-forwarding-reference-overload) Factory requires std
+  // move
+  constexpr Derived(BaseType auto&& other) noexcept
+      : data(std::move(other.data)) {}
+  // NOLINTEND(bugprone-forwarding-reference-overload)
 
   Data data;
 
-  // quick stab at flip member function
-  constexpr auto flip(const Derived&& to_flip) noexcept {
+  // quick stab at Flip member function
+  constexpr auto Flip() noexcept {
     return Derived<Length, -LengthPower, Mass, -MassPower, Time, -TimePower,
-                   Data>(std::forward<decltype(to_flip)>(to_flip));
+                   Data>(std::move(1 / data));
   }
 
   // / operator overloads
@@ -86,14 +113,15 @@ class Derived {
         lhs.data / rhs.data));
   }
 
-  // compound / base
+  // NOLINTBEGIN(bugprone-move-forwarding-reference) Factory requires std move
+  //  compound / base
   friend constexpr auto operator/(Derived lhs, BaseType auto&& rhs) noexcept {
-    return lhs / derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+    return lhs / derived::Factory::Make(std::move(rhs));
   }
 
   // base / compound
   friend constexpr auto operator/(BaseType auto&& lhs, Derived rhs) noexcept {
-    return derived::Factory::Make(std::forward<decltype(lhs)>(lhs)) / rhs;
+    return derived::Factory::Make(std::move(lhs)) / rhs;
   }
 
   // compound /= double
@@ -136,12 +164,12 @@ class Derived {
 
   // compound * base
   friend constexpr auto operator*(Derived lhs, BaseType auto&& rhs) noexcept {
-    return lhs * derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+    return lhs * derived::Factory::Make(std::move(rhs));
   }
 
   // base * compound
   friend constexpr auto operator*(BaseType auto&& lhs, Derived rhs) noexcept {
-    return derived::Factory::Make(std::forward<decltype(lhs)>(lhs)) * rhs;
+    return derived::Factory::Make(std::move(lhs)) * rhs;
   }
 
   // compoud *= double
@@ -181,13 +209,13 @@ class Derived {
 
   // compound + base (I guess a scenario where this happens is rare)
   friend constexpr auto operator+(Derived lhs, BaseType auto&& rhs) noexcept {
-    lhs += derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+    lhs += derived::Factory::Make(std::move(rhs));
     return lhs;
   }
 
   // base + compound (I guess a scenario where this happens is rare?)
   friend constexpr auto operator+(BaseType auto&& lhs, Derived rhs) noexcept {
-    rhs += derived::Factory::Make(std::forward<decltype(lhs)>(
+    rhs += derived::Factory::Make(std::move(
         lhs));  // Is this okay to do with rhs on the left side of the +=?
     return rhs;
   }
@@ -207,7 +235,7 @@ class Derived {
 
   // compound - base (I guess a scenario where this happens is rare)
   friend constexpr auto operator-(Derived lhs, BaseType auto&& rhs) noexcept {
-    lhs -= derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+    lhs -= derived::Factory::Make(std::move(rhs));
     return lhs;
   }
 
@@ -216,22 +244,24 @@ class Derived {
   friend constexpr auto operator-(BaseType auto&& lhs, Derived rhs) noexcept {
     rhs.data *= -1;  // Is this okay to do? I thought it would save more space
                      // than casting lhs to derived then doing -=
-    rhs += derived::Factory::Make(std::forward<decltype(lhs)>(lhs));
+    rhs += derived::Factory::Make(std::move(lhs));
     return rhs;
   }
 };
 
 // base / base
 constexpr auto operator/(BaseType auto&& lhs, BaseType auto&& rhs) noexcept {
-  return derived::Factory::Make(std::forward<decltype(lhs)>(lhs)) /
-         derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+  return derived::Factory::Make(std::move(lhs)) /
+         derived::Factory::Make(std::move(rhs));
 }
 
 // base * base
 constexpr auto operator*(BaseType auto&& lhs, BaseType auto&& rhs) noexcept {
-  return derived::Factory::Make(std::forward<decltype(lhs)>(lhs)) *
-         derived::Factory::Make(std::forward<decltype(rhs)>(rhs));
+  return derived::Factory::Make(std::move(lhs)) *
+         derived::Factory::Make(std::move(rhs));
 }
+
+// NOLINTEND(bugprone-move-forwarding-reference)
 
 // Aliases for basic units
 template <int LP, int MP, int TP>
