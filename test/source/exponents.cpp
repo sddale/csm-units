6565f5f2fa4234e8d06d17e660d6ff81e96e6326
sddale <seth.d.dale@gmail.com>
2023-03-06 15:32:56 -0700

    Now also tests default powers

--- test/source/exponents.cpp
+++ test/source/exponents.cpp
@@ -7,9 +7,9 @@ namespace csm_units::test {
 // NOLINTBEGIN(modernize-use-trailing-return-type)
 TEST_SUITE("Exponents") {
   TEST_CASE("Addition") {
-    CHECK(std::is_same_v<ExponentsAdd<Exponents<1, 2, 3, 1, 1, 1>,
-                                      Exponents<4, 5, 6, 1, 1, 1>>,
-                         Exponents<5, 7, 9, 1, 1, 1>>);
+    CHECK(std::is_same_v<
+          ExponentsAdd<Exponents<1, 2, 3, 1, 1, 1>, Exponents<4, 5, 6>>,
+          Exponents<5, 7, 9, 1, 1, 1>>);
   }
 }
 // NOLINTEND(modernize-use-trailing-return-type)
