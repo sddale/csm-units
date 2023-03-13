1a27772613d648c1b73404281004b0c44bf7845b
manassaini <manas_saini@yahoo.com>
2023-03-13 12:53:42 -0600

    added prefixes file

---
+++ source/csm_units/prefixes.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <csm_units/concepts.hpp>
+#include <ratio>
+
+namespace csm_units {
+
+template <class LP = std::ratio<1, 1>, class MP = std::ratio<1, 1>,
+          class TP = std::ratio<1, 1>>
+struct Prefixes {
+  // Do stuff with prefixes here
+};
+
+}  // namespace csm_units
