d8750ff98e8d350635b6a0e09c00b5c3e9fcdba3
SanderSchott <sschott@mines.edu>
2023-03-29 12:46:25 -0600

    putting a pause on customary

--- source/csm_units/customary.hpp
+++ source/csm_units/customary.hpp
@@ -5,6 +5,17 @@
 namespace csm_units {
 
 template <UnitType SI, RatioType Ratio>
-class Customary {};
+class Customary {
+ public:
+  // constexpr explicit Customary(double value = 0) noexcept : data(value){};
+
+  // // copy constructor
+  // constexpr Customary(const Customary& other) noexcept = default;
+
+  // // move constructor
+  // constexpr Customary(Customary&& other) noexcept = default;
+
+  // double data;  // Come back to this for arbitrary data type
+};
 
 }  // namespace csm_units
