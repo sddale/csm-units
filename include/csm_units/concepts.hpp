b0138c6e34e68306a126ff3d6366764e1de9640e
sddale <seth.d.dale@gmail.com>
2023-04-05 14:16:01 -0600

    differentiated unit and unitbase constraints

--- include/csm_units/concepts.hpp
+++ include/csm_units/concepts.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <__concepts/same_as.h>
+
 #include <concepts>
 #include <cstdint>
 #include <ratio>
@@ -32,36 +34,40 @@ concept Arithmetic = std::is_arithmetic_v<T>;
 
 template <class T>
 concept ExpType = requires(T) {
-                    { T::L::num } -> std::convertible_to<intmax_t>;
-                    { T::L::den } -> std::convertible_to<intmax_t>;
-                    { T::M::num } -> std::convertible_to<intmax_t>;
-                    { T::M::den } -> std::convertible_to<intmax_t>;
-                    { T::T::num } -> std::convertible_to<intmax_t>;
-                    { T::T::den } -> std::convertible_to<intmax_t>;
-                    { T::C::num } -> std::convertible_to<intmax_t>;
-                    { T::C::den } -> std::convertible_to<intmax_t>;
-                    { T::TP::num } -> std::convertible_to<intmax_t>;
-                    { T::TP::den } -> std::convertible_to<intmax_t>;
-                    { T::A::num } -> std::convertible_to<intmax_t>;
-                    { T::A::den } -> std::convertible_to<intmax_t>;
-                    { T::LM::num } -> std::convertible_to<intmax_t>;
-                    { T::LM::den } -> std::convertible_to<intmax_t>;
-                  };
+  { T::L::num } -> std::convertible_to<intmax_t>;
+  { T::L::den } -> std::convertible_to<intmax_t>;
+  { T::M::num } -> std::convertible_to<intmax_t>;
+  { T::M::den } -> std::convertible_to<intmax_t>;
+  { T::T::num } -> std::convertible_to<intmax_t>;
+  { T::T::den } -> std::convertible_to<intmax_t>;
+  { T::C::num } -> std::convertible_to<intmax_t>;
+  { T::C::den } -> std::convertible_to<intmax_t>;
+  { T::TP::num } -> std::convertible_to<intmax_t>;
+  { T::TP::den } -> std::convertible_to<intmax_t>;
+  { T::A::num } -> std::convertible_to<intmax_t>;
+  { T::A::den } -> std::convertible_to<intmax_t>;
+  { T::LM::num } -> std::convertible_to<intmax_t>;
+  { T::LM::den } -> std::convertible_to<intmax_t>;
+};
 
 template <class T>
 concept RatioType = requires(T) {
-                      { T::num } -> std::convertible_to<intmax_t>;
-                      { T::den } -> std::convertible_to<intmax_t>;
-                    };
+  { T::num } -> std::convertible_to<intmax_t>;
+  { T::den } -> std::convertible_to<intmax_t>;
+};
 
 template <class T>
 concept UnitBaseType = requires(T unitbase) {
-                         { unitbase.data } -> std::convertible_to<double>;
-                       };
+  { unitbase.data } -> std::convertible_to<double>;
+  { unitbase.Flip() };
+};
 
 template <class T>
 concept UnitType = requires(T unit) {
-                     { unit.base.data } -> std::convertible_to<double>;
-                   };
+  { unit.data } -> std::convertible_to<double>;
+  // { unitbase.data } -> std::convertible_to<double>;
+  // { unitbase.Flip() };
+  { typename T::SI() };
+};
 
 }  // namespace csm_units
