c3ae31a811f795f516e8368fa5450d929b2f7e5c
manassaini <manas_saini@yahoo.com>
2023-04-17 18:18:36 -0600

    added back the benchmarking

--- test/source/bench.cpp
+++ test/source/bench.cpp
@@ -10,34 +10,34 @@ namespace csm_units::test {
 TEST_SUITE("Benchmarks") {
   TEST_CASE("Addition") {
     // do additions on double
-    // double x = 3.4;
-    // double y = 5.7;
-    // ankerl::nanobench::Bench().minEpochIterations(5000).run(
-    //     "add doubles", [&]() constexpr {
-    //       for (int i = 0; i < 10000; ++i) {
-    //         ankerl::nanobench::doNotOptimizeAway(x + y);
-    //       }
-    //     });
+    double x = 3.4;
+    double y = 5.7;
+    ankerl::nanobench::Bench().minEpochIterations(5000).run(
+        "add doubles", [&]() constexpr {
+          for (int i = 0; i < 10000; ++i) {
+            ankerl::nanobench::doNotOptimizeAway(x + y);
+          }
+        });
 
-    // auto u1 = Length(3.4);
-    // auto u2 = Length(5.7);
-    // ankerl::nanobench::Bench().minEpochIterations(5000).run(
-    //     "add Units", [&]() constexpr {
-    //       for (int i = 0; i < 10000; ++i) {
-    //         ankerl::nanobench::doNotOptimizeAway(u1 + u2);
-    //       }
-    //     });
+    auto u1 = Length(3.4);
+    auto u2 = Length(5.7);
+    ankerl::nanobench::Bench().minEpochIterations(5000).run(
+        "add Units", [&]() constexpr {
+          for (int i = 0; i < 10000; ++i) {
+            ankerl::nanobench::doNotOptimizeAway(u1 + u2);
+          }
+        });
 
-    // auto m1 = Meter(3.4);
-    // auto m2 = Meter(5.7);
-    // ankerl::nanobench::Bench().minEpochIterations(5000).run(
-    //     "add Units", [&]() constexpr {
-    //       for (int i = 0; i < 10000; ++i) {
-    //         ankerl::nanobench::doNotOptimizeAway(m1 + m2);
-    //       }
-    //     });
+    auto m1 = Meter(3.4);
+    auto m2 = Meter(5.7);
+    ankerl::nanobench::Bench().minEpochIterations(5000).run(
+        "add Units", [&]() constexpr {
+          for (int i = 0; i < 10000; ++i) {
+            ankerl::nanobench::doNotOptimizeAway(m1 + m2);
+          }
+        });
 
-    // // do addition on Units
+    // do addition on Units
   }
 }
 
