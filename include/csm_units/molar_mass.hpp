edd5dcac79fe027dba4d10c1b2167b19f08c1f30
SanderSchott <sschott@mines.edu>
2023-06-20 15:52:05 -0600

    forgot to add new files. added molar_mass as a dimension and grams per mole as a unit

---
+++ include/csm_units/molar_mass.hpp
@@ -0,0 +1,10 @@
+/**  \file molar_mass.hpp
+ *   \brief This file provides an easy include for only molar mass units.
+ */
+
+#pragma once
+
+#include "source/csm_units/named_units/molar_mass/dimension.hpp"
+#include "source/csm_units/named_units/molar_mass/grams_per_mole.hpp"
+
+namespace csm_units {}  // namespace csm_units
---
+++ source/csm_units/named_units/molar_mass/grams_per_mole.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../../unit.hpp"
+#include "../../unitcast.hpp"
+#include "dimension.hpp"
+
+namespace csm_units {
+
+using GramsPerMol = Unit<MolarMass, "g/mol", double>;
+
+// Unit Cast for Base GramsPerMol
+template <>
+[[nodiscard]] constexpr auto UnitCast(MolarMass &&input) noexcept
+    -> GramsPerMol {
+  return GramsPerMol(input.data);
+}
+template <>
+[[nodiscard]] constexpr auto UnitCast(GramsPerMol &&input) noexcept
+    -> MolarMass {
+  return MolarMass(input.data);
+}
+
+constexpr auto operator""_gpmol(long double data) noexcept {
+  return GramsPerMol(static_cast<double>(data));
+}
+
+}  // namespace csm_units
