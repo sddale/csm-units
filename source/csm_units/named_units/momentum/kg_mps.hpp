58a00316851fcd9451e0f197d2ba04dc56f0d1b1
Kavin Jey <kavin_jey@Kavins-MacBook-Pro-2.local>
2023-11-13 16:06:37 -0700

    momentum changes

--- source/csm_units/named_units/momentum/kg_mps.hpp
+++ source/csm_units/named_units/momentum/kg_mps.hpp
@@ -1,8 +1,8 @@
-/**  \file kg_mps.hpp
+/**  \file KgMPS.hpp
  *   \brief This file is used to define the Momentum SI unit Kg * m/s.
  *
  *   The following wikipedia page can explain <a
- * href="https://en.wikipedia.org/wiki/Momentum">Gram</a> if needed.
+ * href="https://en.wikipedia.org/wiki/Momentum">Momentum</a> if needed.
  */
 
 #pragma once
@@ -13,26 +13,26 @@
 
 namespace csm_units {
 
-using Kg_mps = Unit<Momentum, "kgmps", double>;
+using KgMPS = Unit<Momentum, "kgmps", double>;
 
 template <>
-[[nodiscard]] constexpr auto UnitCast(Momentum &&input) noexcept -> Kg_mps {
-  return Kg_mps(input.data);
+[[nodiscard]] constexpr auto UnitCast(Momentum &&input) noexcept -> KgMPS {
+  return KgMPS(input.data);
 }
 
 template <>
-[[nodiscard]] constexpr auto UnitCast(Kg_mps &&input) noexcept -> Momentum {
+[[nodiscard]] constexpr auto UnitCast(KgMPS &&input) noexcept -> Momentum {
   return Momentum(input.data);
 }
 
 namespace literals {
 
 constexpr auto operator""_kgmps(long double data) noexcept {
-  return Kg_mps(static_cast<double>(data));
+  return KgMPS(static_cast<double>(data));
 }
 
 constexpr auto operator""_kgmps(unsigned long long data) noexcept {
-  return Kg_mps(static_cast<double>(data));
+  return KgMPS(static_cast<double>(data));
 }
 
 }  // namespace literals
--- source/csm_units/named_units/momentum/slug_ftps.hpp
+++ source/csm_units/named_units/momentum/slug_ftps.hpp
@@ -1,9 +1,9 @@
-/**  \file slug_ftps.hpp
+/**  \file SlugFTPS.hpp
  *   \brief This file is used to define the unit slug * ft/s derived from the
  * base dimension for Momentum.
  *
  *   The following wikipedia page can explain <a
- * href="https://en.wikipedia.org/wiki/Momentum">Gram</a> if needed.
+ * href="https://en.wikipedia.org/wiki/Momentum">Momentum</a> if needed.
  */
 
 #pragma once
@@ -14,26 +14,26 @@
 
 namespace csm_units {
 
-using Slug_ftps = Unit<Momentum, "slugftps", double>;
+using SlugFTPS = Unit<Momentum, "slugftps", double>;
 
 template <>
-[[nodiscard]] constexpr auto UnitCast(Momentum &&input) noexcept -> Slug_ftps {
-  return Kg_mps(input.data / 4.44822162);
+[[nodiscard]] constexpr auto UnitCast(Momentum &&input) noexcept -> SlugFTPS {
+  return SlugFTPS(input.data / 4.44822162);
 }
 
 template <>
-[[nodiscard]] constexpr auto UnitCast(Slug_ftps &&input) noexcept -> Momentum {
+[[nodiscard]] constexpr auto UnitCast(SlugFTPS &&input) noexcept -> Momentum {
   return Momentum(input.data * 4.44822162);
 }
 
 namespace literals {
 
 constexpr auto operator""_slugftps(long double data) noexcept {
-  return Kg_mps(static_cast<double>(data));
+  return SlugFTPS(static_cast<double>(data));
 }
 
 constexpr auto operator"" slugftps(unsigned long long data) noexcept {
-  return Kg_mps(static_cast<double>(data));
+  return SlugFTPS(static_cast<double>(data));
 }
 
 }  // namespace literals
