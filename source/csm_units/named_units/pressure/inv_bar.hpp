b0e22125cb8190d893842bf9b0a9decd0b712124
SanderSchott <sschott@mines.edu>
2023-11-27 15:35:45 -0700

    added inverse pascals and inverse bar units

---
+++ source/csm_units/named_units/pressure/inv_bar.hpp
@@ -0,0 +1,31 @@
+/**  \file bar.hpp
+ *   \brief This file is used to define the unit Inverse Bar derived from the
+ * base dimension for Inverse Pressure.
+ *
+ */
+
+#pragma once
+
+#include "../../unit.hpp"
+#include "../../unitcast.hpp"
+#include "dimension.hpp"
+
+namespace csm_units {
+
+using InvBar = Unit<InvPressure, "inv_bar", double>;
+
+// Unit Cast for Base Pascals to Unit InvBar
+// Conversion Equation: 1 InvBar = 100,000 pascals
+template <>
+[[nodiscard]] constexpr auto UnitCast(InvPressure &&input) noexcept -> InvBar {
+  return InvBar(input.data * 100000);
+}
+
+// Unit Cast for Unit InvBar to Base Pascals
+// Conversion Equation: 1 InvBar = 100,000 pascals
+template <>
+[[nodiscard]] constexpr auto UnitCast(InvBar &&input) noexcept -> InvPressure {
+  return InvPressure(input.data / 100000);
+}
+
+}  // namespace csm_units
