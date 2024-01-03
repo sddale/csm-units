7b5bf4a54fea38b1cdf3257f13e8081cd5ebc546
sddale <seth.d.dale@gmail.com>
2024-01-02 21:23:46 -0700

    temperature tests

---
+++ test/source/named_units/temperature.cpp
@@ -0,0 +1,32 @@
+#include <doctest/doctest.h>
+
+#include <source/csm_units/named_units/thermodynamic_temperature/celsius.hpp>
+#include <source/csm_units/named_units/thermodynamic_temperature/fahrenheit.hpp>
+#include <source/csm_units/named_units/thermodynamic_temperature/kelvin.hpp>
+#include <source/csm_units/named_units/thermodynamic_temperature/rankine.hpp>
+
+#include "../common.hpp"
+
+namespace csm_units::test {
+
+// NOLINTBEGIN(modernize-use-trailing-return-type, misc-use-anonymous-namespace)
+TEST_SUITE("Named Units") {
+  TEST_CASE("Temperature") {
+    CHECK_DBL_EQ(Kelvin(1.).Get(), 1.);
+    CHECK_DBL_NEQ(Kelvin(1.).Get(), 2.);
+    CHECK_DBL_EQ(Kelvin(1.).data, 1.);
+    CHECK_DBL_NEQ(Kelvin(1.).data, 2.);
+
+    CHECK_UNIT_EQ(Kelvin(5. / 9), Rankine(1));
+    CHECK_UNIT_NEQ(Kelvin(5. / 9), Rankine(1));
+
+    CHECK_UNIT_EQ(Kelvin(274.15), Celsius(1));
+    CHECK_UNIT_NEQ(Kelvin(273.15), Celsius(1));
+
+    CHECK_UNIT_EQ(Kelvin(255.928), Fahrenheit(1));
+    CHECK_UNIT_NEQ(Kelvin(255.928), Fahrenheit(1));
+  }
+}
+// NOLINTEND(modernize-use-trailing-return-type, misc-use-anonymous-namespace)
+
+}  // namespace csm_units::test
