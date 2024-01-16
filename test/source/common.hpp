aafb75cadd4c8ae384ae079f1b85182d9508cb43
sddale <seth.d.dale@gmail.com>
2024-01-15 22:12:09 -0700

    formatting

--- test/source/common.hpp
+++ test/source/common.hpp
@@ -10,7 +10,7 @@
 namespace csm_units::test {
 
 template <class UL, class UR>
-requires SameDimensionAs<UL, UR>
+  requires SameDimensionAs<UL, UR>
 constexpr void CHECK_UNIT_EQ(UL lhs, UR rhs) {
   CHECK_EQ(lhs.data, doctest::Approx(rhs.data).scale(1e-6));
 };
